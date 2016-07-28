0e679933057449bdb80dd0e848b7576082c4fc70
daniel_oliver <danieleugeneoliver@gmail.com>
2016-07-28 17:15:27 -0500

    Adding symbol test

--- SqlWhiteboard.Lexcial.Tests/SymbolTests.fs
+++ SqlWhiteboard.Lexcial.Tests/SymbolTests.fs
@@ -11,3 +11,9 @@ let ``Symbol Possibilities has one answer for left parentheses``() =
   let (symbol, punctuation) = possibilities.[0]
   Assert.AreEqual(Symbol.LeftScope, symbol)
   Assert.AreEqual(Punctuation.LeftParentheses, punctuation.[0])
+
+[<Test>]
+let ``Symbol Possibilities has no answer for super crazy punctuation``() = 
+  let symbolList = [| Punctuation.Plus; Punctuation.LeftArrow; Punctuation.DoubleQuote |]
+  let possibilities = SymbolModule.GetPossibilities symbolList
+  Assert.IsEmpty(possibilities)
