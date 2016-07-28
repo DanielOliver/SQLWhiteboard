82e18b87253c0cd9bc51c48ad6180a3c264b8030
daniel_oliver <danieleugeneoliver@gmail.com>
2016-07-28 18:04:51 -0500

    First entry of Tokenizer

--- SqlWhiteboard.Lexical/Token.fs
+++ SqlWhiteboard.Lexical/Token.fs
@@ -1,24 +1,25 @@
 ﻿namespace SqlWhiteboard.Lexical
 
-type TokenType =
+type TokenType = 
   | TSQLKeyword of TSQLKeyword
   | ODBCKeyword of ODBCKeyword
-  | Integer of int
   ///A bracketed identifier for a name
-  | Identifier 
+  | Identifier
   ///An '@' + name 
   | Variable
   ///One or more Punctuation marks that create a symbol
-  | Symbol
+  | Symbol of Symbol
+  ///A single-quote contained string.
+  | String
+  ///Unknown doesn't fit into any other category. Possibly parsing error?  
+  | Unknown
 
-type Token =
+type Token = 
   { ///0-index based
-    LineNumber: int
+    LineNumber : int
     ///0-index based
-    StartColumn: int
+    StartColumn : int
     ///The actual string
-    Token: string
+    Token : string
     ///The type of this token
-    Type: TokenType
-  }
-
+    Type : TokenType }
