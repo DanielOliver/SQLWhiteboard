ddf91cc9b2bc39dc89bbde60ab75f554cfbfcd4b
daniel_oliver <danieleugeneoliver@gmail.com>
2016-07-31 19:02:12 -0500

    Basic Classifier

---
+++ SqlWhiteboard.TSQL/Classifier/TSQLClassifierClassificationDefinition.cs
@@ -0,0 +1,30 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="TSQLClassifierClassificationDefinition.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace SqlWhiteboard.TSQL.Classifier
+{
+    /// <summary>
+    /// Classification type definition export for TSQLClassifier
+    /// </summary>
+    internal static class TSQLClassifierClassificationDefinition
+    {
+        // This disables "The field is never used" compiler's warning. Justification: the field is used by MEF.
+#pragma warning disable 169
+
+        /// <summary>
+        /// Defines the "TSQLClassifier" classification type.
+        /// </summary>
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("TSQLClassifier")]
+        private static ClassificationTypeDefinition typeDefinition;
+
+#pragma warning restore 169
+    }
+}
